af2f6cecd5bf339e409ec36326ca0bf7113808c4
Jiyoon <77226122+yunniverse@users.noreply.github.com>
2021-08-07 04:13:01 +0900

    base_views.py edit delete

--- practice/projects/mysite/pybo/views/base_views.py
+++ practice/projects/mysite/pybo/views/base_views.py
@@ -1,40 +1,48 @@
-# 기능: 기본 관리
-# 함수: index, detail
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-# ---------------------------------- [edit] ---------------------------------- #
-from django.db.models import Q
-# ---------------------------------------------------------------------------- #
+from django.db.models import Q, Count
 
 from ..models import Question
+import logging
+logger = logging.getLogger('pybo')
+
 
 def index(request):
+    logger.info("INFO 레벨로 출력")
     """
-    pybo 목록출력
+    pybo 목록 출력
     """
-    # ---------------------------------- [edit] ---------------------------------- #
-    # 입력 파라미터
+    # 입력 인자
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬기준
+
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(
+            num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(
+            num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
 
     # 조회
-    question_list = Question.objects.order_by('-create_date')
     if kw:
         question_list = question_list.filter(
-            Q(subject__icontains=kw) |  # 제목검색
-            Q(content__icontains=kw) |  # 내용검색
-            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
-            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 작성자 검색
+            Q(answer__author__username__icontains=kw)  # 답변 작성자 검색
         ).distinct()
-    # ---------------------------------------------------------------------------- #
-    # 페이징처리
+
+    # 페이징 처리
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    # ---------------------------------- [edit] ---------------------------------- #
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}  # page와 kw가 추가되었다.
-    # ---------------------------------------------------------------------------- #
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
     return render(request, 'pybo/question_list.html', context)
+
 
 def detail(request, question_id):
     """
