36d68a14d51cc590b265a6f44f5a1e9d7078928f
Jiyoon <77226122+yunniverse@users.noreply.github.com>
2021-08-07 03:27:34 +0900

    models.py 테이블 생성

--- dss/mysite/account/models.py
+++ dss/mysite/account/models.py
@@ -5,5 +5,6 @@ class User(models.Model):
     user_pw = models.CharField(max_length=50, verbose_name='비밀번호')
     user_register_dttm = models.DateTimeField(auto_now_add=True, verbose_name='계정 생성시간')
 
+    # 테이블 생성 클래스
     class Meta:
         db_table = 'user_info'
